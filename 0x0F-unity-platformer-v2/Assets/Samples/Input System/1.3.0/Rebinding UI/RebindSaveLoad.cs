e2ff99ea800d12fdb3655b74989a1c7e3bbce588
peytonbrsmith <peytonbrsmith@gmail.com>
2022-03-15 19:47:39 -0500

    fixed controls not saving

--- 0x0F-unity-platformer-v2/Assets/Samples/Input System/1.3.0/Rebinding UI/RebindSaveLoad.cs
+++ 0x0F-unity-platformer-v2/Assets/Samples/Input System/1.3.0/Rebinding UI/RebindSaveLoad.cs
@@ -8,7 +8,6 @@ public class RebindSaveLoad : MonoBehaviour
     public void OnEnable()
     {
         var rebinds = PlayerPrefs.GetString("rebinds");
-        Debug.Log(rebinds);
         if (!string.IsNullOrEmpty(rebinds))
             actions.LoadBindingOverridesFromJson(rebinds);
     }
@@ -16,7 +15,6 @@ public class RebindSaveLoad : MonoBehaviour
     public void OnDisable()
     {
         var rebinds = actions.SaveBindingOverridesAsJson();
-        Debug.Log(rebinds);
         PlayerPrefs.SetString("rebinds", rebinds);
     }
 }
